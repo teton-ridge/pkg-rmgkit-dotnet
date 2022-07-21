f41fc3add188214d82536c3f4116e363eaeae130
Ken Conner <kconner@bigcountryit.com>
2022-07-21 09:15:36 -0500

    Missing File

---
+++ RMGKit/Models/Account/SubscribeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMGKit.Models.Account
+{
+    public class SubscribeRequest
+    {
+		public SubscribeRequest()
+		{
+			this.Token = String.Empty;
+			this.Product = String.Empty;
+			this.Voucher = String.Empty;
+		}
+		#region Properties
+		public string Token { get; set; }
+		public string Product { get; set; }
+		public string Voucher { get; set; }
+		#endregion
+	}
+}
